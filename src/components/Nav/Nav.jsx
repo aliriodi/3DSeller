4175e187df85fb2909d787bb894951a251ea2c02
Hugo Reyes <chugo940@gmail.com>
2023-01-06 18:20:13 -0600

    Refactor del nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,48 +1,54 @@
 import React from 'react'
-import { useRouter } from 'next/router';
 import Image from 'next/image'
-
 import logo from '../../public/logo.png'
+import Link from 'next/link';
 import home from './house.png'
 
 
 
 function Nav() {
-    const router = useRouter();
     return (
-
-
-
-    
+            
     <>
 		<header>
         {/* <!-- Nav -->  */}
         <div className="nav">
             {/* <!-- Logo -->  */}
-            <a className="nav-logo" onClick={()=>router.push('/')}>
+            <Link href={'/'} legacyBehavior>
+            <a className="nav-logo" >
             <Image src={logo}  alt="13" className='nav-logo_img'/>
                 3D<span className='span1'>SELLER</span>
                 </a>
+            </Link>
             {/* <!-- Nav Icons --> */}
             <div className="nav-icons">
-                <a className="btn" id="bell-icon" onClick={()=>router.push('/productos')}>Productos</a>
+                <Link href={"/productos"} legacyBehavior>
+                <a className="btn" id="bell-icon" >Productos</a>
+                </Link>
             </div>
             <div className="nav-icons">
+                <Link href={"/contacto"} legacyBehavior>
                 <a className="btn" id="bell-icon">Contacto</a>
+                </Link>
             </div>
             <div className="nav-icons">
+                <Link href={"/archivo"} legacyBehavior>
                 <a className="btn" id="bell-icon">Archivo</a>
+                </Link>
             </div>
             <div className="nav-icons">
+                <Link href={"/recursos"}>
                 <a className="btn" id="bell-icon">Recursos</a>
+                </Link>
             </div>
             <div className="nav-icons">
-                <a className="btn" onClick={()=>router.push('/createP')} id="bell-icon">Crear Producto</a>
+                <Link href={"/createP"} legacyBehavior>
+                <a className="btn" id="bell-icon">Crear Producto</a>
+                </Link>
                     </div>
                 </div>
             </header>
         </>
-        // * </div></div> 
     )
 }
 export default Nav
