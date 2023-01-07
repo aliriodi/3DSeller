bfc023b167addb871e006160ad535c7e8a3dc557
Nahuel Escujuri <nahuelescujuri@gmail.com>
2023-01-07 14:20:53 -0300

    Products y a Vcard modificados y con estilos

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -28,19 +28,35 @@ function Products() {
   };
 
   return (
-    <div>
-      <div className="container0">
-        <div>
+    <>
+      <div className="products-container">
+        {/* <div>
           <input
             type="text"
             placeholder="Busca tu producto"
             value={search}
             onChange={onSearchChange}
           />
+        </div> */}
+
+        <div className={`input-container`}>
+            <div className="input-box">
+             <input 
+            type="text"
+            placeholder="Busca tu producto"
+            value={search}
+            onChange={onSearchChange}
+             />
+             <button>BUSCAR</button>
+            </div>
         </div>
-        <div className="flex-container">
+
+
+        {/* Cards Container */}
+        <div className="container-cards">
+          {/* Cards */}
           {productos() &&
-            productos().map((product3d) => {
+          productos().map((product3d) => {
               return (
                 <Vcard
                   key={product3d.name}
@@ -48,13 +64,66 @@ function Products() {
                   name={product3d.name}
                   image={product3d.image}
                   category={product3d.category}
-                  rating='4.3'
+                  rating={product3d.category}
                     />
               );
             })}
+
+
+            {/* Prueba */}
+            <Vcard
+                  key={"Lui"}
+                  id={1}
+                  name={"Lui"}
+                  image={"https://i.pinimg.com/originals/e5/48/0b/e5480bc59916f1e0d66ab1bf9cb14f35.jpg"}
+                  category={"Toys"}
+                  rating='4.3'
+                    />
+            <Vcard
+                  key={"Lui"}
+                  id={1}
+                  name={"Lui"}
+                  image={"https://media.sketchfab.com/models/76430ca6692b4238a54b5d12e90550c5/thumbnails/03258429579f4029a120c89b54d62ebb/d6155d595f4c4af9acfc6146c60f42d7.jpeg"}
+                  category={"Toys"}
+                  rating='4.3'
+                    />
+            
+            <Vcard
+                  key={"Lui"}
+                  id={1}
+                  name={"Lui"}
+                  image={"https://i.pinimg.com/originals/e5/48/0b/e5480bc59916f1e0d66ab1bf9cb14f35.jpg"}
+                  category={"Toys"}
+                  rating='4.3'
+                    />
+            <Vcard
+                  key={"Lui"}
+                  id={1}
+                  name={"Lui"}
+                  image={"https://i.pinimg.com/originals/e5/48/0b/e5480bc59916f1e0d66ab1bf9cb14f35.jpg"}
+                  category={"Toys"}
+                  rating='4.3'
+                    />
+            <Vcard
+                  key={"Lui"}
+                  id={1}
+                  name={"Lui"}
+                  image={"https://i.pinimg.com/originals/e5/48/0b/e5480bc59916f1e0d66ab1bf9cb14f35.jpg"}
+                  category={"Toys"}
+                  rating='4.3'
+                    />
+            
+            <Vcard
+                  key={"Lui"}
+                  id={1}
+                  name={"Lui"}
+                  image={"https://i.pinimg.com/originals/e5/48/0b/e5480bc59916f1e0d66ab1bf9cb14f35.jpg"}
+                  category={"Toys"}
+                  rating='4.3'
+                    />
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/components/Vcard/Vcard.jsx
+++ src/components/Vcard/Vcard.jsx
@@ -2,12 +2,26 @@ import React  from 'react'
 
 export default function Vcard(props) {
   return (
-    <div className="card">
-       <h4 className="cardTitle">{props.name}</h4>
-       {props.image?<a href={`/productos/${props.id}`}>
-      <img  className={props.name==="Alberto Presto Pronta"||props.name==="La Copa del Mundo"?'imageVcardAlberto':"imageVcard"} src={props.image}  alt=""/>  </a>:null}
-      <div className="cardRating">Rating ={' '+props.rating}</div>
-      <div className="cardCategory">Categoria ={' '+props.category}</div>
+    // <div className="card">
+    //    <h4 className="cardTitle">{props.name}</h4>
+    //    {props.image?<a href={`/productos/${props.id}`}>
+    //   <img  className={props.name==="Alberto Presto Pronta"||props.name==="La Copa del Mundo"?'imageVcardAlberto':"imageVcard"} src={props.image}  alt=""/>  </a>:null}
+    //   <div className="cardRating">Rating ={' '+props.rating}</div>
+    //   <div className="cardCategory">Categoria ={' '+props.category}</div>
+    // </div>
+
+    <a a href={`/productos/${props.id}`}>
+      <div className="card" key={props.key}>
+    <img src ={!props.image?null:props.image}/>
+    <div className="card_text">
+    <h3>{props.name}</h3>
+    <b>Rating: {props.rating}</b>
+    <b>Categoria: {props.category}</b>
+    {/* <div className="card_text-type">
+      <p>{props.category}</p>
+    </div> */}
     </div>
+    </div>
+    </a>
   )
 }
