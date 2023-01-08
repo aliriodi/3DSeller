59dd4ce5fb68e4146066f7a49c9c0a9cdb7daf09
aliriodi <aliriodi@gmail.com>
2023-01-07 21:18:39 -0300

    Update Products.jsx

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -31,7 +31,7 @@ function Products() {
     <>
       <div className="products-container">
         {/*
-        ///Anterior version de la Search Bar///
+        ///Anterior version de la Search Bar///*}
         <div>
           <input
             type="text"
