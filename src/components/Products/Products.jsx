9a48334ed8de5b8ba8ca8e0f4c0a846012b2a49c
Hugo Reyes <101015841+Proteinovich@users.noreply.github.com>
2023-01-08 04:33:25 -0600

    intento pushear filtros 2 (#20)
    * intento pushear filtros
    
    * Update package.json
    
    Co-authored-by: aliriodi <aliriodi@gmail.com>

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import Select from "react-select";
 import { getProducts } from "../../redux/DSellerActions";
 import Vcard from "../Vcard/Vcard";
 
@@ -10,29 +11,39 @@ function Products() {
     // eslint-disable-next-line
   }, [dispatch]);
   const { products } = useSelector(state => state.products);
+  console.log(products)
   
  
 
   const productos = () => {
+    if(orden === 2) return render
     if (search !== "") {
-      const filtrados = products.filter((item) =>
+      const busqueda = products.filter((item) =>
         item.name.toLowerCase().includes(search.toLocaleLowerCase())
       );
-      return filtrados;
+      return busqueda;
     } else return products;
   };
 
+  const [orden, setOrden] = useState(0);
+
+  const [render,setRender]= useState("")
+
   const [search, setSearch] = useState("");
   const onSearchChange = () => {
+    setOrden(0)
     setSearch(document.getElementById("sBar").value);
   };
 
+const handleSelectChanges = ({value}) =>{
+   const filtrados = products.filter((item)=> item.category.includes(value[0]))
+   setOrden(2)
+   setRender(filtrados)
+}
+
   return (
-    <>
-      <div className="products-container">
-        {/*
-        Anterior version de la Search Bar
-        
+    <div>
+      <div className="container0">
         <div>
           <input
             type="text"
@@ -41,27 +52,19 @@ function Products() {
           />
           <input type={"button"}
           value={"Buscar"}
-          onClick={onSearchChange}
+          onClick={onSearchChange}          
           />
-        </div>
-        */}
+          <br/>
 
-<div className={`input-container`}>
-            <div className="input-box">
-          <input
-            type="text"
-            placeholder="Ingresa el nombre del producto"
-            id="sBar"
-          />
-             <button onClick={onSearchChange}>BUSCAR</button> 
-            </div>
         </div>
-
-        {/* Cards Container */}
-        <div className="container-cards">
-          {/* Cards */}
-          {productos() &&
-          productos().map((product3d) => {
+        <br/>
+        <div>            
+          <Select options={products.map((p)=>({label: p.category, value: p.category})) }
+          onChange={ handleSelectChanges}/>
+        </div>
+        <div className="flex-container">
+          {products.length > 0 ?
+            productos().map((product3d) => {
               return (
                 <Vcard
                   key={product3d.name}
@@ -69,13 +72,18 @@ function Products() {
                   name={product3d.name}
                   image={product3d.image}
                   category={product3d.category}
-                  rating={product3d.category}
+                  rating='4.3'
                     />
               );
-            })}
+            }):
+            <img
+            className="imagendecarga"
+            src="https://cdn.pixabay.com/animation/2022/07/29/03/42/03-42-18-223_512.gif"
+            alt="imagen de carga"
+            />}
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
