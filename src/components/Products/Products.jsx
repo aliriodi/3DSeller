ee2c0fe6aaa3e7caeac09fa08f4192a01a2a9706
aliriodi <aliriodi@gmail.com>
2023-01-07 22:59:15 -0300

    Update Products.jsx

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,5 +1,5 @@
-//import React, { useEffect, useState } from "react";
-//import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../../redux/DSellerActions";
 import Vcard from "../Vcard/Vcard";
 
