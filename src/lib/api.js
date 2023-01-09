aa8117b4dc8d99000f57e30229ae488cf6ac2504
Jose valencia <javhvac25@outlook.com>
2023-01-09 14:48:15 -0800

    se modifico variable en nodemailer. Correos de contacto vinculados gmail de la empresa

---
+++ src/lib/api.js
@@ -0,0 +1,12 @@
+// Esta es la funcion que se usara en el Submit handler del formulario Contacto
+
+
+export const sendContactForm = async (data) => 
+    fetch("/api/contact", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        }
+    })
