a535ef5a2c70e723e0aef07d6444cdd4730383c6
Alirio Diaz <aliriodi@email.com>
2023-01-11 17:48:42 -0300

    models uauraio 1

---
+++ src/models/User.js
@@ -0,0 +1,31 @@
+// const mongoose = require('mongoose')
+import { Schema, model, models } from 'mongoose';
+
+const UserScheme = new Schema(
+    {
+      name: {
+        type: String,
+        // requires: [true, 'Title is required'],
+        // unique: true,
+        // trim: true,
+        // maxLength: [40, 'Title must be less than 40 characters']
+      },
+      email:{
+        type: String,
+      },
+      favorites:{
+        type: Array,
+      },
+      rol:{
+        type: String,
+        default:'client'
+      },
+      
+},
+    {
+      timestamps: true,
+      versionKey: false
+    }
+  )
+
+  export default models.User || model('User', UserScheme)
--- src/pages/api/auth/[...auth0].js
+++ src/pages/api/auth/[...auth0].js
@@ -1,3 +1,23 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth , handleLogin } from '@auth0/nextjs-auth0';
 
-export default handleAuth();
+export default handleAuth({
+    signup: handleLogin({
+      authorizationParams: { screen_hint: 'signup' }
+    })
+  });
+
+
+// const getLoginState = (req, loginOptions) => {
+//     return { basket_id: getBasketId(req) };
+//   };
+  
+//   export default handleAuth({
+//     async login(req, res) {
+//       try {
+//         await handleLogin(req, res, { getLoginState });
+        
+//       } catch (error) {
+//         res.status(error.status || 500).end();
+//       }
+//     }
+//   });
