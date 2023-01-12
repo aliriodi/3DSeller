58c5932fa1bc05dd077318249bd9ac0f0c9f747d
Alirio Diaz <aliriodi@email.com>
2023-01-12 09:34:53 -0300

    login con preocess.env.AUTH0 el conisimo de la madre con las proces enviroment

--- src/components/LogButton/LogButton.jsx
+++ src/components/LogButton/LogButton.jsx
@@ -25,7 +25,7 @@ export default function LogButton({ handleLogin }) {
           className='btn-logIn'
           id='bell-icon'/>
          <h6> {user.given_name?user.given_name:user.nickname?user.nickname:'Invitado' }</h6>
-        {user.given_name || user.nickname?   <Link href={process.env.AUTH0_BASE_URL+"api/auth/logout"} legacyBehavior>
+        {user.given_name || user.nickname?   <Link href={"https://3dseller.vercel.app/api/auth/logout"} legacyBehavior>
                                                   <div className="container-logout" ><div className="logout-item"> x</div></div>
                                               </Link>:null}
         </div>
