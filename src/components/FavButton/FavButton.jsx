8db9a1446d5006f4f2254a02c38397f7e61cdf2a
Hugo Reyes <chugo940@gmail.com>
2023-01-14 17:07:14 -0600

    cambios_effects_done

--- src/components/FavButton/FavButton.jsx
+++ src/components/FavButton/FavButton.jsx
@@ -1,33 +1,14 @@
 import React, { useState, useEffect, use } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  setFavoritos,
-  chngFavoritos,
-  PutFavorite,
-  getUser,
-} from "../../redux/DSellerActions";
+import { setFavoritos, chngFavoritos } from "../../redux/DSellerActions";
 
 export default function FavButton(props) {
   const dispatch = useDispatch();
 
-  const { user } = useSelector((state) => state.products);
   const { favorites } = useSelector((state) => state.products);
-
-  const sendDB = { favorites: favorites, user: user };
-  //Este Effect busca informacion del Local Storage y si la encuentra carga el arreglo favoritos con ella
-  useEffect(() => {
-    dispatch(getUser());
-
-    const storedFavorites = localStorage.getItem("favorites");
-    if (storedFavorites) {
-      const favorites = JSON.parse(storedFavorites);
-      dispatch(chngFavoritos(favorites));
-    }
-  }, []);
 
   //Una vez cargado el arreglo favoritos con la informacion del local Storage este Effect (que es el mismo que controla y envia los cambios a la DB) envia los nuevos datos a la DB
   useEffect(() => {
-    dispatch(PutFavorite(sendDB));
     const resultado = favorites.find((r) => r.id === props.id);
     if (resultado) {
       setActive(true);
