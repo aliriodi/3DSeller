ca195f025bb155976f710cd1250b1a832f050cdf
Hugo Reyes <chugo940@gmail.com>
2023-01-14 12:02:19 -0600

    storage cleen send

--- src/components/FavButton/FavButton.jsx
+++ src/components/FavButton/FavButton.jsx
@@ -27,7 +27,7 @@ export default function FavButton(props) {
 
   //Una vez cargado el arreglo favoritos con la informacion del local Storage este Effect (que es el mismo que controla y envia los cambios a la DB) envia los nuevos datos a la DB
   useEffect(() => {
-    // dispatch(PutFavorite(sendDB));
+    dispatch(PutFavorite(sendDB));
     const resultado = favorites.find((r) => r.id === props.id);
     if (resultado) {
       setActive(true);
