ee89942fbc0f2e9610373bbcfdee0631b0b78cbb
aliriodi <aliriodi@gmail.com>
2023-01-18 12:43:26 -0300

    Update CardDetail.jsx

--- src/components/CardDetail/CardDetail.jsx
+++ src/components/CardDetail/CardDetail.jsx
@@ -60,7 +60,7 @@ function CardDetail() {
               <PayPalButtons createOrder={async () => {
                 try {
                   const response = await axios({
-                    url: `/api/payment/${productsDetail.price}`,
+                    url: `/api/payment/ATkacPNlx1rEm20wznSCEFxJN9DoXoURPhNGwkz1F8UPdxwcz5fGrtPmtc9OVjyQrp09liKLtK4xntHs`,
                     method: "POST",
                     headers: {
                       "Content-Type": "application/json"
