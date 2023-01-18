c131efb9af23f02bee3913e96b0e4105715e4e03
Nahuel Escujuri <nahuelescujuri@gmail.com>
2023-01-18 20:26:08 -0300

    Review arreglado

--- src/components/CardDetail/CardDetail.jsx
+++ src/components/CardDetail/CardDetail.jsx
@@ -32,6 +32,73 @@ function CardDetail() {
     // eslint-disable-next-line
   }, [id]);
 
+  //#region Agregar Reseña
+  const [commentaryError, setCommentaryError] = useState(false)
+  const [ratingError, setRatingError] =useState(false)
+  const [currentReview, setCurrentReview] = useState({
+    user_email:userL.email,
+    rating: 0,
+    commentary:""
+  })
+
+  useEffect(()=>{
+    setCurrentReview({...currentReview,user_email:userL.email})
+  },[userL])
+
+  const handleReviewChange = (e)=>{
+    if(userL.email != currentReview.user_email)setCurrentReview({...currentReview,user_email:userL.email})
+    setCurrentReview({...currentReview,[e.target.name]:e.target.value})
+  }
+
+  const addReview = ()=>{
+    let newRating = 0;
+
+    // Calcula el Nuevo Rating
+    productsDetail.review?.forEach(rev => {
+      let rating = +rev.rating
+      newRating += rating;
+    })
+    newRating += +currentReview.rating;
+    newRating = (newRating/productsDetail.review?.length);
+
+    //Ver que no haya Errores
+    let ratingLocalError = false
+    let commentLocalError = false
+
+    if(currentReview.rating <= 0){
+      ratingLocalError = true
+    }
+    else ratingLocalError = false
+
+    if(currentReview.commentary.length <= 0){
+      commentLocalError = true
+    }
+    else commentLocalError = false
+
+    setRatingError(ratingLocalError)
+    setCommentaryError(commentLocalError)
+
+    if(ratingLocalError || commentLocalError){
+      return
+    }
+
+    console.log("no tubo error",currentReview)
+    //Se Actualiza el Producto
+    dispatch(putProduct({
+      _id:productsDetail._id,
+      rating:newRating.toFixed(2),
+      review:[
+        ...productsDetail.review,
+        currentReview
+    ]} ))
+
+    setCurrentReview({
+      user_email:userL.email,
+      rating: 0,
+      commentary:""
+    })
+  }
+  //#endregion
 
   return (
     <>
@@ -61,30 +128,6 @@ function CardDetail() {
             <h3>Price:</h3>
             <h3 className="detail-item_item-text">${productsDetail.price}</h3>
           </div>
-          <div style={{ width: "260px", height: "80px", background: "transparent" }}>
-            <PayPalScriptProvider options={{ "client-id": 'ATkacPNlx1rEm20wznSCEFxJN9DoXoURPhNGwkz1F8UPdxwcz5fGrtPmtc9OVjyQrp09liKLtK4xntHs' }}>
-              <PayPalButtons createOrder={async () => {
-                try {
-                  const response = await axios({
-                    url: `/api/payment/${products.Detail.price}`,
-                    method: "POST",
-                    headers: {
-                      "Content-Type": "application/json"
-                    }
-                  })
-                  return response.data.id
-                } catch(error) {
-                  console.log(error)
-                }
-              }} 
-              onCancel={(data)=> console.log("Compra Cancelada")} 
-              onApprove={(data, actions) => {
-                console.log(data)
-                actions.order.capture
-              }}
-              style={{ layout: "horizontal", color: "black" }} />
-            </PayPalScriptProvider>
-          </div>
         </div>
       </div>
 
@@ -92,35 +135,6 @@ function CardDetail() {
       <div className="data-descripcion">
         <p>{productsDetail.description}</p>
       </div>
-
-      <div style={{ maxWidth: "750px", minHeight: "200px" }}>
-        <PayPalScriptProvider
-          clientId={
-            "ATkacPNlx1rEm20wznSCEFxJN9DoXoURPhNGwkz1F8UPdxwcz5fGrtPmtc9OVjyQrp09liKLtK4xntHs"
-          }
-          onError={(error) => console.log(error)}
-          options={{
-            "client-id": "test",
-            components: "buttons",
-            currency: "USD",
-          }}
-        >
-          <ButtonWrapper currency={currency} showSpinner={false} />
-        </PayPalScriptProvider>
-      </div>
-
-      {/* <PayPalButtons                                      
-          createOrder={createOrder}
-          onApprove={onApprove}
-          onCancel={onCancel}
-        /> */}
-      {/* <PayPalScriptProvider
-        clientId={'ATkacPNlx1rEm20wznSCEFxJN9DoXoURPhNGwkz1F8UPdxwcz5fGrtPmtc9OVjyQrp09liKLtK4xntHs'}
-        onError={(error) => console.log(error)}
-      >
-       
-        
-      </PayPalScriptProvider> */}
 
       {/* Formulario de Reseñas */}
       <form className={`review-form`}>
