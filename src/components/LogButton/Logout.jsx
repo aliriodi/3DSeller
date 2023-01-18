e22d9a54c495d6101ba924e08285b17017e04499
Alirio Diaz <aliriodi@email.com>
2023-01-18 19:41:21 -0300

    acomodado logout , son dos botnes uno de login que llama al usuario y uno de logout que no llama nadie porque si lo llama no deja hacer logout

--- src/components/LogButton/Logout.jsx
+++ src/components/LogButton/Logout.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { useRouter } from "next/router";
+import perfilIcon from "./perfil-icon_default.png";
+import Image from "next/image";
+import Link from "next/link";
 
-export default function Logout({ handleLogout }) {
+export default function Logout() {
+    const { push } = useRouter();
+    function handleLogout (){ push("/api/auth/logout")};
+    const {  userL  } = useSelector((state) => state.products);
     return (
         <>
-            <div className="nav-icons nav-icons_logIn">
-                <button onClick={handleLogout}>LOGOUT</button>
+           
+      <div  className="nav-icons nav-icons_logIn">
+        <Image
+          src={perfilIcon}
+          alt="perfil"
+          className="btn-logIn"
+          id="bell-icon"
+        />
+        <h6>
+          {userL.name}
+        </h6>
+          <Link
+            href={"/api/auth/logout"}
+            legacyBehavior >
+            <div  onClick={()=>handleLogout()} className="container-logout">
+              <div className="logout-item"> x</div>
             </div>
+          </Link>
+         </div>
         </>
     )
 }
