16b112f6cb6e36bb795f0cf7201799f434ca5841
Hugo Reyes <chugo940@gmail.com>
2023-01-18 22:56:12 -0600

    cambiosenuser

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,32 +1,56 @@
 import React from "react";
-
+import {
+  AiFillTwitterCircle,
+  AiFillInstagram,
+  AiFillLinkedin,
+  AiFillFacebook,
+} from "react-icons/ai";
 export default function Footer() {
   return (
-    <footer className="row-1" id="row-1">
-      <div className="">
-        <h2> Nosotros </h2>
-        <p>
-          Es su centro de producción de fabricación aditiva de piezas impresas
-          en 3D. Con más de 30 materiales para cualquier aplicación, obtendrá
-          las piezas que necesita con solo contratarnos.
-        </p>
-      </div>
-      <br></br>
-      <div className="">
-        <h2> Coontacto </h2>
-        <p>seller3d@gmail.com</p>
-        <p>+xx xxx xxxxx xxxx</p>
-        <p>calle xxxxxxx xxxxxx</p>
-      </div>
-      <br />
-      <div className="">
-        <h2> Redes sociales </h2>
-        <ul>
-          <li> Facebook </li>
-          <li> Instagram </li>
-          <li> Linkedin </li>
-          <li> Twitter </li>
-        </ul>
+    <footer className="row-1" id="">
+      <div className="row2">
+        <div className="">
+          <h2 className=""> Nosotros </h2>
+          <p>
+            3DSeller es una empresa con alto compromiso con los clientes y con
+            la capacidad tecnica necesaria para poder hacer sus necesidades
+            realidad sin importar la complejidad de su diseño
+          </p>
+        </div>
+        <br></br>
+        <div className="">
+          <h2> Contacto </h2>
+          <ul>
+            <li> +543516132710 </li>
+            <li> threedseller@gmail.com </li>
+            <li>
+              Argentina - Cordoba Capital Barrio Pueyrredon CP X5004 Calle Fray
+              Mamerto Esquiu 1431
+            </li>
+          </ul>
+        </div>
+        <br />
+        <div className="">
+          <h2> Redes sociales </h2>
+          <ul>
+            <div>
+              <AiFillFacebook />
+              <li> Facebook </li>
+            </div>
+            <div>
+              <AiFillInstagram />
+              <li> Instagram </li>
+            </div>
+            <div>
+              <AiFillLinkedin />
+              <li> Linkedin </li>
+            </div>
+            <div>
+              <AiFillTwitterCircle />
+              <li> Twitter </li>
+            </div>
+          </ul>
+        </div>
       </div>
     </footer>
   );
