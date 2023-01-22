8613a49bb1238dafdff07f2c7bc3989781546a85
Hugo Reyes <chugo940@gmail.com>
2023-01-22 16:03:53 -0600

    agregado solo una reseña

--- src/components/Vcard/Vcard.jsx
+++ src/components/Vcard/Vcard.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import FavButton from "../FavButton/FavButton";
 import { useSelector } from "react-redux";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Vcard(props) {
   const { user } = useSelector((state) => state.products);
@@ -19,14 +21,16 @@ export default function Vcard(props) {
             category={props.category}
           />
         )}
-        <a href={`/productos/${props.id}`}>
-          <img src={!props.image ? null : props.image} />
-          <div className="card_text">
-            <h3>{props.name}</h3>
-            <b>Rating: {props.rating}</b>
-            <b>Categoria: {props.category}</b>
-          </div>
-        </a>
+        <Link href={`/productos/${props.id}`} legacyBehavior>
+          <a>
+            <img src={!props.image ? null : props.image} />
+            <div className="card_text">
+              <h3>{props.name}</h3>
+              <b>Rating: {props.rating}</b>
+              <b>Categoria: {props.category}</b>
+            </div>
+          </a>
+        </Link>
       </div>
     </div>
   );
