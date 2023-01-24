a3c4856a7bddffe286351dd4bde49150f0e6633d
Alirio Diaz <aliriodi@email.com>
2023-01-24 14:13:38 -0300

    favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,7 +15,6 @@ export default function App({ Component, pageProps }: AppProps) {
           <title>3DSeller </title>
           {/* <link rel="shortcut icon" type="image/png" href="/logo.png" /> */}
           <link rel="shortcut icon" href="/images/favicon.ico" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
           <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
         </Head>
